29eff3f92a29f16f6109f9c629b0f2db359ea0be
Jaeger580 <jaegerj253@gmail.com>
2026-06-25 18:11:48 -0400

    Progress
    Almost have tags moving

--- Assets/Original Assets/Scripts/Game Systems/Anomaly Spawn & Report/Report/Tool Tag Selector.cs
+++ Assets/Original Assets/Scripts/Game Systems/Anomaly Spawn & Report/Report/Tool Tag Selector.cs
@@ -3,17 +3,27 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Splines;
 
 public class ToolTagSelector : MonoBehaviour
 {
     [SerializeField] private AnomalyRayCheck taggerSpawner;
     [SerializeField] private TagSet tags;
 
+    // Tags prefabs we will spawn and manipulate
+    [SerializeField] private GameObject textPrefab;
+
+    // spline we will animate along
+    [SerializeField] private SplineContainer spline;
+
+    // Canvas that we will insantiate objects under
+    [SerializeField] private GameObject tagsCanvas;
+    private List<GameObject> activeTags = new List<GameObject>();
 
     // HARD CODED FOR NOW
-    [SerializeField] TMP_Text textOne;
-    [SerializeField] TMP_Text textTwo;
-    [SerializeField] TMP_Text textThree;
+    //[SerializeField] TMP_Text textOne;
+    //[SerializeField] TMP_Text textTwo;
+    //[SerializeField] TMP_Text textThree;
 
 
     // Tracks which tag we currently have selected
@@ -21,12 +31,34 @@ public class ToolTagSelector : MonoBehaviour
 
     private void Start()
     {
+        InitializeTags();
+
         // initiliaze
         ChangeTagType(0);
 
         // Display first three tags on the device
         UpdateText();
 
+    }
+
+    // Instantiates ui objects with the correct text based on the TagSet 
+    private void InitializeTags() 
+    {
+        activeTags.Clear();
+
+        foreach (Tag tag in tags.items) 
+        {
+            var newObject = Instantiate(textPrefab, tagsCanvas.transform);
+            TMP_Text textObj = newObject.GetComponentInChildren<TMP_Text>();
+            SplineAnimate aniObj = newObject.GetComponent<SplineAnimate>();
+
+            textObj.text = tag.humanReadableTagIdentifier;
+            aniObj.Container = spline;
+            aniObj.ObjectForwardAxis = SplineComponent.AlignAxis.NegativeZAxis;
+            aniObj.ElapsedTime = 0.0f;
+
+            activeTags.Add(newObject);
+        }
     }
 
     // change active tag
@@ -41,33 +73,77 @@ public class ToolTagSelector : MonoBehaviour
     // update the text on the device
     private void UpdateText()
     {
+        int topTag;
         int firstTag;
         int secondTag;
         int thirdTag;
+        int bottomTag;
 
         // check if we need to loop
         if (currentTagIndex - 1 < 0)
         {
             firstTag = tags.items.Count - 1;
-            secondTag = currentTagIndex;
-            thirdTag = currentTagIndex + 1;
+            topTag = tags.items.Count - 2;
         }
-        else if (currentTagIndex + 1 > tags.items.Count - 1)
+        else if (currentTagIndex - 2 < 0)
         {
+            topTag = tags.items.Count - 1;
             firstTag = currentTagIndex - 1;
-            secondTag = currentTagIndex;
-            thirdTag = 0;
         }
         else 
         {
             firstTag = currentTagIndex - 1;
-            secondTag = currentTagIndex;
-            thirdTag = currentTagIndex + 1;
+            topTag = currentTagIndex - 2;
         }
 
-        textOne.text = tags.items[firstTag].humanReadableTagIdentifier;
-        textTwo.text = tags.items[secondTag].humanReadableTagIdentifier;
-        textThree.text = tags.items[thirdTag].humanReadableTagIdentifier;
+        if (currentTagIndex + 1 > tags.items.Count - 1)
+        {
+            thirdTag = 0;
+            bottomTag = tags.items.Count - 1;
+        }
+        else if(currentTagIndex + 2 > tags.items.Count - 1)
+        {
+            thirdTag = currentTagIndex + 1;
+            bottomTag = 0;
+        }
+        else 
+        {
+            thirdTag = currentTagIndex + 1;
+            bottomTag = currentTagIndex + 2;
+        }
+
+        secondTag = currentTagIndex;
+        //// check if we need to loop
+        //if (currentTagIndex - 1 < 0)
+        //{
+        //    firstTag = tags.items.Count - 1;
+        //    secondTag = currentTagIndex;
+        //    thirdTag = currentTagIndex + 1;
+        //}
+        //else if (currentTagIndex + 1 > tags.items.Count - 1)
+        //{
+        //    firstTag = currentTagIndex - 1;
+        //    secondTag = currentTagIndex;
+        //    thirdTag = 0;
+        //}
+        //else 
+        //{
+        //    firstTag = currentTagIndex - 1;
+        //    secondTag = currentTagIndex;
+        //    thirdTag = currentTagIndex + 1;
+        //}
+
+        //textOne.text = tags.items[firstTag].humanReadableTagIdentifier;
+        //textTwo.text = tags.items[secondTag].humanReadableTagIdentifier;
+        //textThree.text = tags.items[thirdTag].humanReadableTagIdentifier;
+        activeTags[topTag].GetComponent<SplineAnimate>().ElapsedTime = 1.0f;
+        activeTags[firstTag].GetComponent<SplineAnimate>().ElapsedTime = 0.75f;
+        activeTags[secondTag].GetComponent<SplineAnimate>().ElapsedTime = 0.50f;
+        activeTags[thirdTag].GetComponent<SplineAnimate>().ElapsedTime = 0.25f;
+        activeTags[bottomTag].GetComponent<SplineAnimate>().ElapsedTime = 0.0f;
+
+        //activeTags[activeTags.Count - 1].GetComponent<SplineAnimate>().ElapsedTime = 1.0f;
+
 
     }
 
@@ -79,6 +155,12 @@ public class ToolTagSelector : MonoBehaviour
             // Get the new index after shifting.
             // SHOULD LOOP AROUND ON IT OWN IN THEORY.
             int indexChange = (currentTagIndex + 1) % tags.items.Count;
+
+            foreach (GameObject obj in activeTags) 
+            {
+                SplineAnimate ani = obj.GetComponent<SplineAnimate>();
+                obj.GetComponent<SplineAnimate>().ElapsedTime += 0.25f;
+            }
 
             ChangeTagType(indexChange);
         }
