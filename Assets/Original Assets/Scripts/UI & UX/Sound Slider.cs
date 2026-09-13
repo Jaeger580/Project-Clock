7868075048e407b494f17b0dc1d61443d08f4fa4
Jaeger580 <jaegerj253@gmail.com>
2026-09-12 21:13:40 -0400

    Sound Overhaul Again
    Rebalanced most of the audio un the game.
    Many sounds have had their pitch slightly reduced.
    Found alternate noises for Howie.

---
+++ Assets/Original Assets/Scripts/UI & UX/Sound Slider.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+// Terrible code for setting volume, but works for now.
+// Would be better to convert this script to handle arrays of all the sliders,
+// instead multiple  copies of this script are needed for each slider.
+
+
+public class SoundSlider : MonoBehaviour
+{
+    [SerializeField] Slider soundSlider;
+    [SerializeField] AudioMixerGroup soundMixer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    private void SetVolume(float volume) 
+    {
+        if(volume < 1) 
+        {
+            volume = .001f;
+        }
+
+        RefreshSlider(volume);
+        PlayerPrefs.SetFloat(soundMixer.ToString(), volume);
+        soundMixer.audioMixer.SetFloat(soundMixer.ToString(), Mathf.Log10(volume / 100) * 20);
+    }
+
+    public void SetVolumeFromSlider() 
+    {
+        SetVolume(soundSlider.value);
+    }
+
+    public void RefreshSlider(float volume)
+    {
+        soundSlider.value = volume;
+    }
+}
